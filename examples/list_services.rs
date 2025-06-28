20b33e5e74d07672f304b8d4ab3a34b47e9964d2
David Ventura <davidventura27@gmail.com>
2025-06-28 16:18:26 +0200

    add ipvsadm -ln example

--- examples/list_services.rs
+++ examples/list_services.rs
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MIT
-
 use netlink_packet_core::{NetlinkMessage, NetlinkPayload, NLM_F_REQUEST};
 use netlink_packet_generic::{
     ctrl::{nlas::GenlCtrlAttrs, GenlCtrl, GenlCtrlCmd},
@@ -8,7 +6,7 @@ use netlink_packet_generic::{
 use netlink_packet_ipvs::ctrl::{
     nlas::{
         destination::{DestinationExtended, ForwardTypeFull},
-        service::{Protocol, Scheduler, Service},
+        service::{Protocol, Scheduler, Service, SvcCtrlAttrs},
         IpvsCtrlAttrs,
     },
     IpvsCtrlCmd, IpvsServiceCtrl,
@@ -111,91 +109,66 @@ fn format_forward_method(method: &ForwardTypeFull) -> &'static str {
     }
 }
 
-fn list_services(socket: &mut Socket, family_id: u16) {
-    println!("Listing IPVS services and destinations...");
+fn query_destinations_for_service(
+    socket: &mut Socket,
+    family_id: u16,
+    service_attrs: &[SvcCtrlAttrs],
+) {
+    // Filter service attributes to only include what's needed for destination query
+    // Remove netmask and other attributes that might cause serialization issues
+    let filtered_attrs: Vec<SvcCtrlAttrs> = service_attrs
+        .iter()
+        .filter(|attr| match attr {
+            SvcCtrlAttrs::AddressFamily(_)
+            | SvcCtrlAttrs::Protocol(_)
+            | SvcCtrlAttrs::AddrBytes(_)
+            | SvcCtrlAttrs::Port(_)
+            | SvcCtrlAttrs::Fwmark(_) => true,
+            _ => false,
+        })
+        .cloned()
+        .collect();
 
     let ctrl_msg = IpvsServiceCtrl {
-        cmd: IpvsCtrlCmd::GetService,
-        nlas: vec![],
+        cmd: IpvsCtrlCmd::GetDest,
+        nlas: vec![IpvsCtrlAttrs::Service(filtered_attrs)],
         family_id,
     };
 
-    let serialized = ctrl_msg.serialize(true); // true = dump flag
+    let serialized = ctrl_msg.serialize(true); // dump flag
     socket.send(&serialized, 0).unwrap();
 
-    println!("IP Virtual Server version 1.2.1 (size=4096)");
-    println!("Prot LocalAddress:Port Scheduler Flags");
-    println!(
-        "  -> RemoteAddress:Port           Forward Weight ActiveConn InActConn"
-    );
-
-    // Read all responses until NLMSG_DONE
     loop {
         let (rxbuf, _addr) = socket.recv_from_full().unwrap();
 
         match NetlinkMessage::<GenlMessage<IpvsServiceCtrl>>::deserialize(
             &rxbuf,
         ) {
-            Ok(response) => match response.payload {
-                NetlinkPayload::Error(err) => {
-                    if let Some(code) = err.code {
-                        let errno = code.get();
-                        if errno == -1 {
-                            eprintln!(
-                                "Permission denied. Try running with sudo."
-                            );
-                            return;
-                        } else {
-                            eprintln!("Error listing services: {}", errno);
-                            return;
-                        }
-                    }
-                }
-                NetlinkPayload::Done(_) => {
-                    break;
-                }
-                NetlinkPayload::InnerMessage(genlmsg) => {
-                    let ipvs_msg = &genlmsg.payload;
-
-                    for nla in &ipvs_msg.nlas {
-                        match nla {
-                            IpvsCtrlAttrs::Service(service_attrs) => {
-                                match Service::from_nlas(service_attrs) {
-                                    Ok(service_ext) => {
-                                        let service = &service_ext.service;
-                                        let stats = &service_ext.stats64;
-
-                                        println!(
-                                            "{} {} {} [{}]",
-                                            format_protocol(service.protocol),
-                                            format_address_port(
-                                                service.address,
-                                                service.port
-                                            ),
-                                            format_scheduler(service.scheduler),
-                                            format!("{:08x}", service.flags.0)
-                                        );
-
-                                        println!("  Service stats: {} conns, {} pkts in, {} pkts out",
-                                                stats.connections,
-                                                stats.incoming_packets,
-                                                stats.outgoing_packets
-                                            );
-                                    }
-                                    Err(e) => {
-                                        eprintln!(
-                                            "Failed to parse service: {}",
-                                            e
-                                        );
-                                    }
-                                }
+            Ok(response) => {
+                match response.payload {
+                    NetlinkPayload::Error(err) => {
+                        if let Some(code) = err.code {
+                            if code.get() != -2 {
+                                // -2 = ENOENT (no destinations)
+                                eprintln!(
+                                    "Error querying destinations: {}",
+                                    code.get()
+                                );
                             }
-                            IpvsCtrlAttrs::Destination(dest_attrs) => {
+                        }
+                        break;
+                    }
+                    NetlinkPayload::Done(_) => {
+                        break;
+                    }
+                    NetlinkPayload::InnerMessage(genlmsg) => {
+                        for nla in &genlmsg.payload.nlas {
+                            if let IpvsCtrlAttrs::Destination(dest_attrs) = nla
+                            {
                                 match DestinationExtended::from_nlas(dest_attrs)
                                 {
                                     Ok(dest_ext) => {
                                         let dest = &dest_ext.destination;
-
                                         println!(
                                             "  -> {} {} {} {} {}",
                                             format_address_port(
@@ -220,14 +193,99 @@ fn list_services(socket: &mut Socket, family_id: u16) {
                             }
                         }
                     }
+                    _ => break,
                 }
-                _ => {
-                    println!("Unexpected response type");
+            }
+            Err(_) => break,
+        }
+    }
+}
+
+fn list_services(socket: &mut Socket, family_id: u16) {
+    println!("Listing IPVS services and destinations...");
+
+    let ctrl_msg = IpvsServiceCtrl {
+        cmd: IpvsCtrlCmd::GetService,
+        nlas: vec![],
+        family_id,
+    };
+
+    let serialized = ctrl_msg.serialize(true); // true = dump flag
+    socket.send(&serialized, 0).unwrap();
+
+    println!("IP Virtual Server version 1.2.1 (size=4096)");
+    println!("Prot LocalAddress:Port Scheduler Flags");
+    println!(
+        "  -> RemoteAddress:Port           Forward Weight ActiveConn InActConn"
+    );
+
+    let mut services = Vec::new();
+
+    // Read all service responses until NLMSG_DONE
+    loop {
+        let (rxbuf, _addr) = socket.recv_from_full().unwrap();
+
+        match NetlinkMessage::<GenlMessage<IpvsServiceCtrl>>::deserialize(
+            &rxbuf,
+        ) {
+            Ok(response) => {
+                match response.payload {
+                    NetlinkPayload::Error(err) => {
+                        if let Some(code) = err.code {
+                            let errno = code.get();
+                            if errno == -1 {
+                                eprintln!(
+                                    "Permission denied. Try running with sudo."
+                                );
+                                return;
+                            } else {
+                                eprintln!("Error listing services: {}", errno);
+                                return;
+                            }
+                        }
+                    }
+                    NetlinkPayload::Done(_) => {
+                        break; // End of service dump
+                    }
+                    NetlinkPayload::InnerMessage(genlmsg) => {
+                        for nla in &genlmsg.payload.nlas {
+                            if let IpvsCtrlAttrs::Service(service_attrs) = nla {
+                                services.push(service_attrs.clone());
+                            }
+                        }
+                    }
+                    _ => {
+                        println!("Unexpected response type");
+                    }
                 }
-            },
+            }
             Err(e) => {
                 eprintln!("Failed to parse response: {:?}", e);
                 break;
+            }
+        }
+    }
+
+    for service_attrs in services {
+        match Service::from_nlas(&service_attrs) {
+            Ok(service_ext) => {
+                let service = &service_ext.service;
+                println!(
+                    "{} {} {} [{}]",
+                    format_protocol(service.protocol),
+                    format_address_port(service.address, service.port),
+                    format_scheduler(service.scheduler),
+                    format!("{:08x}", service.flags.0)
+                );
+
+                query_destinations_for_service(
+                    socket,
+                    family_id,
+                    &service_attrs,
+                );
+            }
+            Err(e) => {
+                eprintln!("Failed to parse service: {}", e);
             }
         }
     }
@@ -239,6 +297,6 @@ fn main() {
     socket.connect(&SocketAddr::new(0, 0)).unwrap();
 
     let family_id = get_ipvs_family_id(&mut socket);
+
     list_services(&mut socket, family_id);
 }
-
